0dd8c3adf1b0335d7fb4e18b23d67e0f149331c0
Abhirup154 <pingking29705@gmail.com>
2024-04-26 07:48:12 +0530

    changed

--- app.js
+++ app.js
@@ -9,7 +9,9 @@ const cors = require('cors')
 //setting middleware
 app.use(express.json())
 app.use(require('./router/auth'))
-app.use(cors())
+app.use(cors({
+    origin:"https://recipe-app-api-tv4c.onrender.com"
+}))
 
 const PORT = process.env.PORT || 8000
 
