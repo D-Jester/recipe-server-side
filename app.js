c05adf378c5f73e588313275297a46edaa539636
Abhirup154 <pingking29705@gmail.com>
2024-04-26 23:32:06 +0530

    changed

--- app.js
+++ app.js
@@ -7,9 +7,6 @@ const app = express()
 const cors = require('cors')
 
 //setting middleware
-// header('Access-Control-Allow-Origin : *')
-// header('Access-Control-Allow-Origin : POST, GET, OPTIONS, PUT, DELETE')
-// header('Access-Control-Allow-Origin : Content-type, X-Auth-Token, Origin, Authorisation')
 
 app.use(cors({
     origin:["https://recipe-app-2be3.onrender.com"]
